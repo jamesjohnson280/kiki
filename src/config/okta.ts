5a157fa915762eb0befadb307c608c713f17ca6f
Jim Johnson <james.johnson280@gmail.com>
2024-03-30 13:21:33 -0400

    Add config

---
+++ src/config/okta.ts
@@ -0,0 +1,17 @@
+const clientId = '0oag4omgq9N6bMWRb5d7';
+const oktaDomain = 'dev-86310623.okta.com';
+
+export const Okta = {
+  oidc: {
+    issuer: `https://${oktaDomain}/oauth2/default`,
+    clientId,
+    scopes: ['openid', 'profile', 'email'],
+    redirectUri: `${window.location.origin}/login/callback`
+  },
+  widget: {
+    issuer: `https://${oktaDomain}/oauth2/default`,
+    clientId,
+    redirectUri: `${window.location.origin}/login/callback`,
+    scopes: ['openid', 'profile', 'email'],
+  }
+};
