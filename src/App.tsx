f2f0fe1eae01e752604ac22b2c1645ee7ca135a5
Jim Johnson <james.johnson280@gmail.com>
2024-04-09 09:17:11 -0400

    Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { Routes } from '@/config/Routes';
+import { Routes } from '@/config';
 
 export const App = () => {
   const router = createBrowserRouter(Routes);
---
+++ src/config/Okta.ts
@@ -0,0 +1,17 @@
+const domain = 'dev-86310623.okta.com';
+const clientId = '0oag4omgq9N6bMWRb5d7';
+
+export default {
+  oidc: {
+    issuer: `https://${domain}/oauth2/default`,
+    clientId: `${clientId}`,
+    scopes: ['openid', 'profile', 'email'],
+    redirectUri: `${window.location.origin}/login/callback`
+  },
+  widget: {
+    issuer: `https://${domain}/oauth2/default`,
+    clientId: `${clientId}`,
+    redirectUri: `${window.location.origin}/login/callback`,
+    scopes: ['openid', 'profile', 'email'],
+  }
+};
