1a6e0150fb9bfe356c06b596a20c034bea08fd31
Jim Johnson <james.johnson280@gmail.com>
2024-04-09 10:06:12 -0400

    Get sign in working

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,8 +1,24 @@
-import { Nav } from '@/components';
+import { useNavigate } from "react-router-dom";
+import { useOktaAuth } from "@okta/okta-react";
+import { OktaWidget } from "@/components/OktaWidget";
 
-export const Login = () => (
-  <div>
-    <Nav />
-    <h1>Login</h1>
-  </div>
-);
+export const Login = () => {
+  const { oktaAuth, authState } = useOktaAuth();
+  const navigate = useNavigate();
+
+  const onSuccess = (tokens) => {
+    oktaAuth.handleLoginRedirect(tokens);
+  };
+
+  const onError = (err) => {
+    console.log('Sign in error:', err);
+  };
+
+  if (!authState) {
+    return <div>Loading ... </div>;
+  }
+
+  return authState.isAuthenticated ?
+    navigate('/') :
+    <OktaWidget onSuccess={onSuccess} onError={onError}/>;
+};
